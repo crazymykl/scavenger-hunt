88e6ad57a4f1b89ebfcea01e2f352b7a5d3b8a60
Mike MacDonald <crazymykl@gmail.com>
2025-02-08 12:45:00 -0500

    Switch from bulma to mantine
    The former doesn't integrate with react and css modules out-of-the-box.

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,21 @@
 import "./App.css"
+import { ColorSchemeScript, MantineProvider } from "@mantine/core"
 import { Counter } from "./features/counter/Counter"
 import logo from "./logo.svg"
 
 const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Counter />
-      </header>
-    </div>
+    <>
+      <ColorSchemeScript defaultColorScheme="auto" />
+      <MantineProvider defaultColorScheme="auto">
+        <div className="App">
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <Counter />
+          </header>
+        </div>
+      </MantineProvider>
+    </>
   )
 }
 
