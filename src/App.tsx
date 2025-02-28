ff9798d5f653715c5e824dbcf249b086b7e81c2c
Mike MacDonald <crazymykl@gmail.com>
2025-02-27 23:56:10 -0500

    Speedup transitions in tests

--- src/App.tsx
+++ src/App.tsx
@@ -3,20 +3,39 @@ import { Navigate, Route, Routes, useParams } from "react-router"
 import { Hunt } from "./features/hunt/Hunt"
 import { ThemeToggle } from "./features/theme/ThemeToggle"
 
-const ItemDetailsHelper = () => {
+const ItemDetailsHelper = ({
+  transitionDuration,
+}: {
+  transitionDuration: number
+}) => {
   const { id, code } = useParams()
 
-  return <Hunt.ItemDetails id={id!} code={code} />
+  return (
+    <Hunt.ItemDetails
+      id={id!}
+      code={code}
+      transitionDuration={transitionDuration}
+    />
+  )
 }
 
-const App = () => {
+const App = ({
+  transitionDuration = 1000,
+}: {
+  transitionDuration?: number
+}) => {
   return (
     <div className="App">
       <header className="App-header">
         <ThemeToggle />
         <Routes>
           <Route path="/" element={<Hunt />}>
-            <Route path="/find/:id/:code?" element={<ItemDetailsHelper />} />
+            <Route
+              path="/find/:id/:code?"
+              element={
+                <ItemDetailsHelper transitionDuration={transitionDuration} />
+              }
+            />
           </Route>
           <Route path="*" element={<Navigate to="/" replace />} />
         </Routes>
