5bcc5ab08e14774e8b12ff13b79e81cc29671ef3
Mike MacDonald <crazymykl@gmail.com>
2025-07-13 17:00:37 -0400

    Set hunt items grid cols based on viewport orientation

--- src/features/hunt/Hunt.tsx
+++ src/features/hunt/Hunt.tsx
@@ -1,6 +1,6 @@
 import { SimpleGrid, Stack } from "@mantine/core"
 import { Outlet } from "react-router"
-import { useAppSelector } from "../../app/hooks"
+import { useAppSelector, useWindowDimensions } from "../../app/hooks"
 import { huntSlice } from "./huntSlice"
 import { Item } from "./Item"
 import { ItemDetails } from "./ItemDetails"
@@ -8,13 +8,14 @@ import { ItemDetails } from "./ItemDetails"
 const CELL_SPACING = 5
 
 export const Hunt = () => {
+  const { height, width } = useWindowDimensions()
   const itemIds = useAppSelector(huntSlice.selectors.selectItemIds)
   const listItems = itemIds.map(id => <Hunt.Item id={id} key={id} />)
 
   return (
     <Stack align="center">
       <SimpleGrid
-        cols={4}
+        cols={width > height ? 6 : 3}
         spacing={CELL_SPACING}
         verticalSpacing={CELL_SPACING}
       >
