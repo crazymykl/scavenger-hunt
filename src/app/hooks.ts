6ff2a9beadf6f350dfdff6a5134a84024100bd12
Mike MacDonald <crazymykl@gmail.com>
2025-07-13 17:00:37 -0400

    Set hunt items grid cols based on viewport orientation

--- src/app/hooks.ts
+++ src/app/hooks.ts
@@ -5,8 +5,31 @@
 // for importing and re-exporting the typed versions of hooks.
 /* eslint-disable @typescript-eslint/no-restricted-imports */
 import { useDispatch, useSelector } from "react-redux"
+import { useState, useEffect } from "react"
 import type { AppDispatch, RootState } from "./store"
 
 // Use throughout your app instead of plain `useDispatch` and `useSelector`
 export const useAppDispatch = useDispatch.withTypes<AppDispatch>()
 export const useAppSelector = useSelector.withTypes<RootState>()
+
+export const useWindowDimensions = () => {
+  const [windowDimensions, setWindowDimensions] = useState(
+    getWindowDimensions(),
+  )
+
+  useEffect(() => {
+    const handleResize = () => {
+      setWindowDimensions(getWindowDimensions())
+    }
+
+    window.addEventListener("resize", handleResize)
+    return () => window.removeEventListener("resize", handleResize)
+  }, [])
+
+  return windowDimensions
+}
+
+const getWindowDimensions = () => ({
+  width: window.innerWidth,
+  height: window.innerHeight,
+})
