9249ee5bce216312b9c7853543d7901554aabc28
Mike MacDonald <crazymykl@gmail.com>
2025-08-02 22:27:36 -0400

    Bedazzle the "View Reward" Button until first click

---
+++ src/features/ActionButton.tsx
@@ -0,0 +1,22 @@
+import { Button } from "@mantine/core"
+import { Link } from "react-router"
+import styles from "./ActionButton.module.css"
+import { useState } from "react"
+
+export const ActionButton = ({ done }: { done: boolean }) => {
+  const [seen, setSeen] = useState(false)
+
+  return done ? (
+    <Link
+      to="/reward"
+      className={seen ? undefined : styles.glowingAura}
+      onClick={() => setSeen(true)}
+    >
+      <Button color="green">View Reward</Button>
+    </Link>
+  ) : (
+    <Link to="/scan">
+      <Button>Scan</Button>
+    </Link>
+  )
+}
