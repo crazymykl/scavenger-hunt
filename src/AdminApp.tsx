166a8c0466502228ef996d1ca5bc58f882f793ff
Mike MacDonald <crazymykl@gmail.com>
2025-08-14 11:47:13 -0400

    Add QR code for intro to Admin section

--- src/AdminApp.tsx
+++ src/AdminApp.tsx
@@ -1,24 +1,42 @@
-import { AppShell, Center, Group } from "@mantine/core"
+import { useState } from "react"
+import { AppShell, Card, Center, Group, Stack, Switch } from "@mantine/core"
+import { QRCodeSVG } from "qrcode.react"
 
 import { AdminHunt } from "./features/hunt/admin/AdminHunt"
+import { makeAbsoluteUrl } from "./features/hunt/lib"
 import { ThemeToggle } from "./features/theme/ThemeToggle"
 import { useGetHuntQuery, useGetShadowQuery } from "./services/api"
 
 const AdminApp = () => {
   const { data: hunt, isLoading } = useGetHuntQuery(undefined)
   const { data: shadow } = useGetShadowQuery(undefined)
+  const [showHunt, setShowHunt] = useState(false)
 
   return (
     <AppShell header={{ height: 48 }}>
       <AppShell.Header>
         <Group h="100%" px="md" justify="flex-end">
+          <Switch
+            labelPosition="left"
+            label="Show Items"
+            defaultChecked={showHunt}
+            onChange={e => setShowHunt(e.currentTarget.checked)}
+            disabled={!hunt || !shadow}
+          />
           <ThemeToggle />
         </Group>
       </AppShell.Header>
       <AppShell.Main>
         <Center>
-          {isLoading || !hunt || !shadow ? (
+          {isLoading ? (
             "Loading..."
+          ) : !showHunt || !hunt || !shadow ? (
+            <Card withBorder mt={12}>
+              <Stack>
+                <QRCodeSVG value={makeAbsoluteUrl("/intro")} size={325} />
+                <Center>Scan to Begin</Center>
+              </Stack>
+            </Card>
           ) : (
             <AdminHunt hunt={hunt} shadow={shadow} />
           )}
